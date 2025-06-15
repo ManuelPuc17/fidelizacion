3809ad4c0ee2ae4a50baaa9105ac3b0b048d0152
ManuelPuc17 <manuelstylepuc17@gmail.com>
2025-06-15 16:30:47 -0600

    Crud de beneficios

--- view/admin/footer.php
+++ view/admin/footer.php
@@ -1,6 +1,7 @@
     </div> <!-- cierre del container -->
 
     <!-- Opcional: Footer -->
+     
     <footer class="text-center py-4 bg-dark text-white mt-auto">
         &copy; <?= date('Y') ?> Sistema de Fidelización - Admin
     </footer>
